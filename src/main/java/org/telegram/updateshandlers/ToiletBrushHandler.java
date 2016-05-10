44e50b4ce2f2892316b29906659ea00489ca8ed6
AMUNOZ <AMUNOZ@10.166.81.37>
2016-05-10 10:58:36 +0200

    Doc folder

--- src/main/java/org/telegram/updateshandlers/ToiletBrushHandler.java
+++ src/main/java/org/telegram/updateshandlers/ToiletBrushHandler.java
@@ -121,7 +121,6 @@ public class ToiletBrushHandler extends TelegramLongPollingBot {
 			return Mensajes.hablarMessage;
 		else
 			return Mensajes.noExisteComandoMessage;
-
 	}
 
 	private boolean isStart(String mensaje) {
